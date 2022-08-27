c24d6b07b99b85045f5bf0f86b2514e5076e1978
Liu Xing Stream <liuxingcanghai@gmail.com>
2022-08-27 15:20:33 +0700

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ LOGGER_ID = int(getenv("LOGGER_ID"))
 MONGO_DB_URI = getenv("MONGO_DB_URI")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/c5952790fa8235f499749.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/27b618e6006ed4f3804c3.jpg")
 START_IMG = getenv("START_IMG")
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
